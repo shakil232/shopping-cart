46e41bd1c575a2687062c222082bf8e622eebc1f
unknown <dxshakil2@gmail.com>
2021-01-31 21:41:40 -0800

    hoynai

--- js/shopping.js
+++ js/shopping.js
@@ -1,18 +1,13 @@
 // phonehandelar
 const phoneIncrease = document.getElementById('phone-increase').addEventListener('click', function(){
     phoneadd(true);
-    // const phoneCount = document.getElementById('phone-count');
-    // const phoneNumber = parseInt(phoneCount.value);
-    // const phoneNewNumber = phoneNumber +1;
-    // phoneCount.value = phoneNewNumber;
-    // const phoneTotal = phoneNewNumber *1219;
-    // document.getElementById('phone-total').innerText = '$' +phoneTotal;
+    
 });
 
 const phoneDecrease = document.getElementById('phone-decrease').addEventListener('click', function(){
     phoneadd(false); 
 
-})
+});
 function phoneadd (isadd){
     const phoneCount = document.getElementById('phone-count');
     const phoneNumber = parseInt(phoneCount.value);
@@ -27,6 +22,7 @@ function phoneadd (isadd){
     phoneCount.value = phoneNewNumber;
     const phoneTotal = phoneNewNumber *1219;
     document.getElementById('phone-total').innerText = '$' +phoneTotal;
+    calculetTotal();
 }
 
 // const phoneIncrease = document.getElementById('phone-increase').addEventListener('click', function(){
@@ -71,11 +67,8 @@ function handlePrudactItem (isincrase){
     caseInput.value = caseNewCount ;
     let caseTotal = caseNewCount *59;
     document.getElementById('case-total').innerText= '$' +caseTotal;
+    calculetTotal();
 }
-
-
-
-
 
 // let caseButton = document.getElementById('CaseButton').addEventListener('click', function(){
 //     let caseInput = document.getElementById('case-count');
@@ -93,3 +86,26 @@ function handlePrudactItem (isincrase){
 //     let caseTotal = caseNewCount *59;
 //     document.getElementById('case-total').innerText= '$' +caseTotal;
 // })
+
+// calculettotal
+function calculetTotal(){
+    const phoneInput = document.getElementById('phone-count');
+    const phoneCount = parseInt(phoneInput.value);
+
+    
+
+    const totalPrice = phoneCount*1219 + caseCount*59;
+    document.getElementById('total-price').innerText='&' +totalPrice;
+
+    const tex = math.round(totalPrice/10);
+    document.getElementById('tax-amount').innerText ='&' +tex;
+
+    const grandTotal = totalPrice+tex;
+    document.getElementById('grand-total').innerText='$'+grandTotal ;
+    
+}
+function inputadd(){
+    let caseInput = document.getElementById('case-count');
+    let caseCount = parseInt(caseInput.value);
+    return caseCount;
+}
