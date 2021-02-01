f4d586d682c460bf3b8da1fa4f012b3b92841b2e
unknown <dxshakil2@gmail.com>
2021-01-31 20:35:59 -0800

    add

--- js/shopping.js
+++ js/shopping.js
@@ -1,5 +1,55 @@
+// phonehandelar
+const phoneIncrease = document.getElementById('phone-increase').addEventListener('click', function(){
+    phoneadd(true);
+    // const phoneCount = document.getElementById('phone-count');
+    // const phoneNumber = parseInt(phoneCount.value);
+    // const phoneNewNumber = phoneNumber +1;
+    // phoneCount.value = phoneNewNumber;
+    // const phoneTotal = phoneNewNumber *1219;
+    // document.getElementById('phone-total').innerText = '$' +phoneTotal;
+});
 
-let caseButton = document.getElementById('CaseButton').addEventListener('click', function(){
+const phoneDecrease = document.getElementById('phone-decrease').addEventListener('click', function(){
+    phoneadd(false); 
+
+})
+function phoneadd (isadd){
+    const phoneCount = document.getElementById('phone-count');
+    const phoneNumber = parseInt(phoneCount.value);
+    // const phoneNewNumber = phoneNumber -1;
+    let phoneNewNumber = phoneNumber ;
+    if(isadd==true){
+        phoneNewNumber = phoneNumber +1;
+    }
+    if(isadd==false && phoneNumber >0){
+        phoneNewNumber= phoneNumber -1;
+    }
+    phoneCount.value = phoneNewNumber;
+    const phoneTotal = phoneNewNumber *1219;
+    document.getElementById('phone-total').innerText = '$' +phoneTotal;
+}
+
+// const phoneIncrease = document.getElementById('phone-increase').addEventListener('click', function(){
+//     const phoneCount = document.getElementById('phone-count');
+//     const phoneNumber = parseInt(phoneCount.value);
+//     const phoneNewNumber = phoneNumber +1;
+//     phoneCount.value = phoneNewNumber;
+//     const phoneTotal = phoneNewNumber *1219;
+//     document.getElementById('phone-total').innerText = '$' +phoneTotal;
+// });
+
+// const phoneDecrease = document.getElementById('phone-decrease').addEventListener('click', function(){
+//     const phoneCount = document.getElementById('phone-count');
+//     const phoneNumber = parseInt(phoneCount.value);
+//     const phoneNewNumber = phoneNumber -1;
+//     phoneCount.value = phoneNewNumber;
+//     const phoneTotal = phoneNewNumber *1219;
+//     document.getElementById('phone-total').innerText = '$' +phoneTotal;
+
+// })
+
+// casehandelar
+let caseButton = document.getElementById('Case-incres').addEventListener('click', function(){
  handlePrudactItem(true);
 });
 let caseDcrase = document.getElementById('case-decrase').addEventListener('click', function(){
@@ -9,7 +59,7 @@ let caseDcrase = document.getElementById('case-decrase').addEventListener('click
 function handlePrudactItem (isincrase){
     let caseInput = document.getElementById('case-count');
     let caseCount = parseInt(caseInput.value);
-    // let caseNewCount= caseCount -1;
+   
     let caseNewCount= caseCount;
     if(isincrase==true){
         caseNewCount= caseCount +1;
@@ -17,6 +67,7 @@ function handlePrudactItem (isincrase){
     if(isincrase==false && caseCount >0){
         caseNewCount= caseCount -1;
     }
+    
     caseInput.value = caseNewCount ;
     let caseTotal = caseNewCount *59;
     document.getElementById('case-total').innerText= '$' +caseTotal;
